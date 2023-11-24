fd460b8761eeb643b17430d64dcbf8c31d008fb4
mia <cosmalatina@163.com>
2023-11-24 23:51:13 +0800

    lv2Plus基本完成

--- s4.3_package_lv2plus.go
+++ s4.3_package_lv2plus.go
@@ -11,13 +11,13 @@ import (
 
 func main() {
 	// 打开一个日志文件，如果文件不存在则创建，追加写入
-	loggerTxt, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE, 0666)
+	loggerTxt, err := os.OpenFile("logPlus.txt", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println("文件打开失败！", err)
 	}
 	defer loggerTxt.Close()
 	// 创建一个带时间戳的写入器
-	logWriter := &timestampWriter{
+	logWriter := &timestampWriterS{
 		timestamp: time.Now(),
 		of:        loggerTxt,
 	}
@@ -30,28 +30,32 @@ func main() {
 	fmt.Fprintln(logWriter, "用户执行操作B")
 }
 
-func BytesCombine(pBytes ...[]byte) []byte {
+func BytesCombineF(pBytes ...[]byte) []byte {
 	return bytes.Join(pBytes, []byte(""))
 }
 
 // timestampWriter 是一个实现 io.Writer 接口的结构体，它在写入数据前添加时间戳
-type timestampWriter struct {
+type timestampWriterS struct {
 	timestamp time.Time
 	of        *os.File
 }
 
-func (tw *timestampWriter) Write(p []byte) (n int, err error) {
+func (tw *timestampWriterS) Write(p []byte) (n int, err error) {
 	// 添加时间戳和时间
 	//println(tw.timestamp.Unix())
 	var clock [3]int
 	clockStr := []byte("")
 	clock[0], clock[1], clock[2] = tw.timestamp.Clock()
-	for _, v := range clock {
+	for i, v := range clock {
 		clockStr = strconv.AppendInt(clockStr, int64(v), 10)
+		if i != 2 {
+			clockStr = BytesCombineF(clockStr, []byte(":"))
+		}
 	}
 	stamp := []byte(strconv.FormatInt(tw.timestamp.Unix(), 10))
-	p = BytesCombine([]byte("时间："), clockStr, []byte("时间戳："), stamp, p)
+	p = BytesCombineF([]byte("时间："), clockStr, []byte("时间戳："), stamp, p)
 	//p = append(p, '\n')
+
 	// 输出到文件
 	combinedWriter := io.MultiWriter(tw.of, os.Stderr)
 	n, err = combinedWriter.Write(p)
